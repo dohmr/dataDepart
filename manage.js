48d6d506999fee75237aa2b43e125579fd7e7d49
doh <theshawns0n@gmail.com>
2020-10-13 13:00:33 -0400

    pseudo

---
+++ manage.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer")
+
+/*
+mysql
+inquirer
+console.table
+
+connection
+
+prompts 
+    View
+        Employees / managers
+        Department / roles
+    Add
+        Employee / managers
+        department / role
+    Update
+        Employees / managers
+        Department / roles   
+
+        use case/switch to prompt and act
+
